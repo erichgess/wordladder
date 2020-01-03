133bab298710ea15b3c0d44e4879c4b173e7d979
Erich Gerhard Ess <erich.g.ess@gmail.com>
2020-01-03 11:13:01 -0500

    setup profiling.

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to this file")
 
 func main() {
+	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	g := LoadDictionary("./words")
 
-	p := g.Path(g.Find("zzz"), g.Find("zzz"))
+	v1 := g.Find("dog")
+	v2 := g.Find("cat")
+	var p []int
+	for i := 0; i < 1000000; i++ {
+		p = g.Path(v1, v2)
+	}
 
 	for _, v := range p {
 		fmt.Println(g.vertices[v].word)
