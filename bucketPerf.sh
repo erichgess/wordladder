a32750e00d283544f70e94b3a552395f5559c2b7
Erich Gerhard Ess <erich.g.ess@gmail.com>
2020-01-20 11:11:43 -0500

    Add an analysis spreadsheet of how perf breaks down as bucket size changes
    Loading Dictionary
    Avg sz:29.807053
    Words: 999999	Edges: 3164481
    Num Buckets: 8388608
    buildIndex time (sec): 1.650018
    buildAdjList time (sec): 2.941645
    LoadDictionary time (sec): 4.791766
    
    Profile: ./profiles/stats-collector/2020-01-20_11-11-43
    
    File: wordladder
    Type: alloc_space
    Time: Jan 20, 2020 at 11:11am (EST)
    Showing nodes accounting for 816.90MB, 99.86% of 818.05MB total
    Dropped 1 node (cum <= 4.09MB)
          flat  flat%   sum%        cum   cum%
      380.14MB 46.47% 46.47%   380.14MB 46.47%  main.(*Graph).buildAdjList
         192MB 23.47% 69.94%      192MB 23.47%  main.newIndex
      151.25MB 18.49% 88.43%   816.90MB 99.86%  main.LoadDictionary
       93.50MB 11.43% 99.86%    93.50MB 11.43%  main.(*index).add
             0     0% 99.86%   818.05MB   100%  main.main
             0     0% 99.86%   818.05MB   100%  runtime.main

--- bucketPerf.sh
+++ bucketPerf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 100000 | tail -n 2
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 250000 | tail -n 1
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 500000 | tail -n 1
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 750000 | tail -n 1
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 1000000 | tail -n 1
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 2500000 | tail -n 1
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 5000000 | tail -n 1
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 7500000 | tail -n 1
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 10000000 | tail -n 1
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 25000000 | tail -n 1
-./wordladder --dict ./dicts/million.dict --index-stats --csv --num-buckets 50000000 | tail -n 1
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 100000 | tail -n 2
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 250000 | tail -n 1
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 500000 | tail -n 1
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 750000 | tail -n 1
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 1000000 | tail -n 1
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 2500000 | tail -n 1
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 5000000 | tail -n 1
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 7500000 | tail -n 1
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 10000000 | tail -n 1
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 25000000 | tail -n 1
+./wordladder --dict ./dicts/million.dict --csv --num-buckets 50000000 | tail -n 1
